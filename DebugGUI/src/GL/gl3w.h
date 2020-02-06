26df1a25f172c3aa6ebd58c46b2ce63bff1a8ae2
David Rohr <drohr@jwdt.org>
2020-02-06 10:50:07 +0100

    Bump gl3w headers to newest version

--- DebugGUI/src/GL/gl3w.h
+++ DebugGUI/src/GL/gl3w.h
@@ -1,37 +1,35 @@
 /*
-
-    This file was generated with gl3w_gen.py, part of gl3w
-    (hosted at https://github.com/skaslev/gl3w)
-
-    This is free and unencumbered software released into the public domain.
-
-    Anyone is free to copy, modify, publish, use, compile, sell, or
-    distribute this software, either in source code form or as a compiled
-    binary, for any purpose, commercial or non-commercial, and by any
-    means.
-
-    In jurisdictions that recognize copyright laws, the author or authors
-    of this software dedicate any and all copyright interest in the
-    software to the public domain. We make this dedication for the benefit
-    of the public at large and to the detriment of our heirs and
-    successors. We intend this dedication to be an overt act of
-    relinquishment in perpetuity of all present and future rights to this
-    software under copyright law.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-    IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
-
-*/
+ * This file was generated with gl3w_gen.py, part of gl3w
+ * (hosted at https://github.com/skaslev/gl3w)
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #ifndef __gl3w_h_
 #define __gl3w_h_
 
-#include "glcorearb.h"
+#include <GL/glcorearb.h>
 
 #ifndef __gl_h_
 #define __gl_h_
@@ -41,7 +39,12 @@
 extern "C" {
 #endif
 
-typedef void (*GL3WglProc)();
+#define GL3W_OK 0
+#define GL3W_ERROR_INIT -1
+#define GL3W_ERROR_LIBRARY_OPEN -2
+#define GL3W_ERROR_OPENGL_VERSION -3
+
+typedef void (*GL3WglProc)(void);
 typedef GL3WglProc (*GL3WGetProcAddressProc)(const char *proc);
 
 /* gl3w api */
@@ -52,7 +55,7 @@ GL3WglProc gl3wGetProcAddress(const char *proc);
 
 /* gl3w internal state */
 union GL3WProcs {
-	GL3WglProc ptr[653];
+	GL3WglProc ptr[659];
 	struct {
 		PFNGLACTIVESHADERPROGRAMPROC                            ActiveShaderProgram;
 		PFNGLACTIVETEXTUREPROC                                  ActiveTexture;
@@ -218,6 +221,7 @@ union GL3WProcs {
 		PFNGLFLUSHMAPPEDBUFFERRANGEPROC                         FlushMappedBufferRange;
 		PFNGLFLUSHMAPPEDNAMEDBUFFERRANGEPROC                    FlushMappedNamedBufferRange;
 		PFNGLFRAMEBUFFERPARAMETERIPROC                          FramebufferParameteri;
+		PFNGLFRAMEBUFFERPARAMETERIMESAPROC                      FramebufferParameteriMESA;
 		PFNGLFRAMEBUFFERRENDERBUFFERPROC                        FramebufferRenderbuffer;
 		PFNGLFRAMEBUFFERTEXTUREPROC                             FramebufferTexture;
 		PFNGLFRAMEBUFFERTEXTURE1DPROC                           FramebufferTexture1D;
@@ -267,6 +271,7 @@ union GL3WProcs {
 		PFNGLGETFRAGDATALOCATIONPROC                            GetFragDataLocation;
 		PFNGLGETFRAMEBUFFERATTACHMENTPARAMETERIVPROC            GetFramebufferAttachmentParameteriv;
 		PFNGLGETFRAMEBUFFERPARAMETERIVPROC                      GetFramebufferParameteriv;
+		PFNGLGETFRAMEBUFFERPARAMETERIVMESAPROC                  GetFramebufferParameterivMESA;
 		PFNGLGETGRAPHICSRESETSTATUSPROC                         GetGraphicsResetStatus;
 		PFNGLGETINTEGER64I_VPROC                                GetInteger64i_v;
 		PFNGLGETINTEGER64VPROC                                  GetInteger64v;
@@ -399,9 +404,11 @@ union GL3WProcs {
 		PFNGLMINSAMPLESHADINGPROC                               MinSampleShading;
 		PFNGLMULTIDRAWARRAYSPROC                                MultiDrawArrays;
 		PFNGLMULTIDRAWARRAYSINDIRECTPROC                        MultiDrawArraysIndirect;
+		PFNGLMULTIDRAWARRAYSINDIRECTCOUNTPROC                   MultiDrawArraysIndirectCount;
 		PFNGLMULTIDRAWELEMENTSPROC                              MultiDrawElements;
 		PFNGLMULTIDRAWELEMENTSBASEVERTEXPROC                    MultiDrawElementsBaseVertex;
 		PFNGLMULTIDRAWELEMENTSINDIRECTPROC                      MultiDrawElementsIndirect;
+		PFNGLMULTIDRAWELEMENTSINDIRECTCOUNTPROC                 MultiDrawElementsIndirectCount;
 		PFNGLNAMEDBUFFERDATAPROC                                NamedBufferData;
 		PFNGLNAMEDBUFFERSTORAGEPROC                             NamedBufferStorage;
 		PFNGLNAMEDBUFFERSUBDATAPROC                             NamedBufferSubData;
@@ -428,6 +435,7 @@ union GL3WProcs {
 		PFNGLPOINTSIZEPROC                                      PointSize;
 		PFNGLPOLYGONMODEPROC                                    PolygonMode;
 		PFNGLPOLYGONOFFSETPROC                                  PolygonOffset;
+		PFNGLPOLYGONOFFSETCLAMPPROC                             PolygonOffsetClamp;
 		PFNGLPOPDEBUGGROUPPROC                                  PopDebugGroup;
 		PFNGLPRIMITIVERESTARTINDEXPROC                          PrimitiveRestartIndex;
 		PFNGLPROGRAMBINARYPROC                                  ProgramBinary;
@@ -507,6 +515,7 @@ union GL3WProcs {
 		PFNGLSHADERBINARYPROC                                   ShaderBinary;
 		PFNGLSHADERSOURCEPROC                                   ShaderSource;
 		PFNGLSHADERSTORAGEBLOCKBINDINGPROC                      ShaderStorageBlockBinding;
+		PFNGLSPECIALIZESHADERPROC                               SpecializeShader;
 		PFNGLSTENCILFUNCPROC                                    StencilFunc;
 		PFNGLSTENCILFUNCSEPARATEPROC                            StencilFuncSeparate;
 		PFNGLSTENCILMASKPROC                                    StencilMask;
@@ -877,6 +886,7 @@ extern union GL3WProcs gl3wProcs;
 #define glFlushMappedBufferRange                         gl3wProcs.gl.FlushMappedBufferRange
 #define glFlushMappedNamedBufferRange                    gl3wProcs.gl.FlushMappedNamedBufferRange
 #define glFramebufferParameteri                          gl3wProcs.gl.FramebufferParameteri
+#define glFramebufferParameteriMESA                      gl3wProcs.gl.FramebufferParameteriMESA
 #define glFramebufferRenderbuffer                        gl3wProcs.gl.FramebufferRenderbuffer
 #define glFramebufferTexture                             gl3wProcs.gl.FramebufferTexture
 #define glFramebufferTexture1D                           gl3wProcs.gl.FramebufferTexture1D
@@ -926,6 +936,7 @@ extern union GL3WProcs gl3wProcs;
 #define glGetFragDataLocation                            gl3wProcs.gl.GetFragDataLocation
 #define glGetFramebufferAttachmentParameteriv            gl3wProcs.gl.GetFramebufferAttachmentParameteriv
 #define glGetFramebufferParameteriv                      gl3wProcs.gl.GetFramebufferParameteriv
+#define glGetFramebufferParameterivMESA                  gl3wProcs.gl.GetFramebufferParameterivMESA
 #define glGetGraphicsResetStatus                         gl3wProcs.gl.GetGraphicsResetStatus
 #define glGetInteger64i_v                                gl3wProcs.gl.GetInteger64i_v
 #define glGetInteger64v                                  gl3wProcs.gl.GetInteger64v
@@ -1058,9 +1069,11 @@ extern union GL3WProcs gl3wProcs;
 #define glMinSampleShading                               gl3wProcs.gl.MinSampleShading
 #define glMultiDrawArrays                                gl3wProcs.gl.MultiDrawArrays
 #define glMultiDrawArraysIndirect                        gl3wProcs.gl.MultiDrawArraysIndirect
+#define glMultiDrawArraysIndirectCount                   gl3wProcs.gl.MultiDrawArraysIndirectCount
 #define glMultiDrawElements                              gl3wProcs.gl.MultiDrawElements
 #define glMultiDrawElementsBaseVertex                    gl3wProcs.gl.MultiDrawElementsBaseVertex
 #define glMultiDrawElementsIndirect                      gl3wProcs.gl.MultiDrawElementsIndirect
+#define glMultiDrawElementsIndirectCount                 gl3wProcs.gl.MultiDrawElementsIndirectCount
 #define glNamedBufferData                                gl3wProcs.gl.NamedBufferData
 #define glNamedBufferStorage                             gl3wProcs.gl.NamedBufferStorage
 #define glNamedBufferSubData                             gl3wProcs.gl.NamedBufferSubData
@@ -1087,6 +1100,7 @@ extern union GL3WProcs gl3wProcs;
 #define glPointSize                                      gl3wProcs.gl.PointSize
 #define glPolygonMode                                    gl3wProcs.gl.PolygonMode
 #define glPolygonOffset                                  gl3wProcs.gl.PolygonOffset
+#define glPolygonOffsetClamp                             gl3wProcs.gl.PolygonOffsetClamp
 #define glPopDebugGroup                                  gl3wProcs.gl.PopDebugGroup
 #define glPrimitiveRestartIndex                          gl3wProcs.gl.PrimitiveRestartIndex
 #define glProgramBinary                                  gl3wProcs.gl.ProgramBinary
@@ -1166,6 +1180,7 @@ extern union GL3WProcs gl3wProcs;
 #define glShaderBinary                                   gl3wProcs.gl.ShaderBinary
 #define glShaderSource                                   gl3wProcs.gl.ShaderSource
 #define glShaderStorageBlockBinding                      gl3wProcs.gl.ShaderStorageBlockBinding
+#define glSpecializeShader                               gl3wProcs.gl.SpecializeShader
 #define glStencilFunc                                    gl3wProcs.gl.StencilFunc
 #define glStencilFuncSeparate                            gl3wProcs.gl.StencilFuncSeparate
 #define glStencilMask                                    gl3wProcs.gl.StencilMask
