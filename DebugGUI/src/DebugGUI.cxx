76ca1a66a55841f868ff0e4a1822a0813d9c1491
Luigi Fusco <luigifusco98@gmail.com>
2021-07-21 17:20:58 +0200

    Add function to get JSON of draw data and record a video

--- DebugGUI/src/DebugGUI.cxx
+++ DebugGUI/src/DebugGUI.cxx
@@ -9,6 +9,9 @@
 #include <GLFW/glfw3.h>
 #include <cstdio>
 #include <functional>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
 static void default_error_callback(int error, const char* description)
 {
@@ -51,7 +54,78 @@ void* initGUI(const char* name, void(*error_callback)(int, char const*descriptio
   io.Fonts->AddFontFromMemoryTTF((void*)s_iconsFontAwesomeTtf, sizeof(s_iconsFontAwesomeTtf), 12.0f, &icons_config, icons_ranges);
 
   ImPlot::CreateContext();
+
   return window;
+}
+
+// returns a string containing drawing data in JSON format
+// the returned JSON is composed of a list of draw commands
+std::string getFrameJSON(ImDrawData *draw_data) {
+  std::ostringstream json_data;
+
+  json_data << "[";
+
+  for (int cmd_id = 0; cmd_id < draw_data->CmdListsCount; ++cmd_id) {
+    const auto cmd_list = draw_data->CmdLists[cmd_id];
+    const auto vtx_buffer = cmd_list->VtxBuffer;
+    const auto idx_buffer = cmd_list->IdxBuffer;
+    const auto cmd_buffer = cmd_list->CmdBuffer;
+
+  
+    json_data << "{\"vtx\":[";
+    for (int i = 0; i < vtx_buffer.size(); ++i) {
+      auto v = vtx_buffer[i];
+      json_data << "[" << v.pos.x << "," << v.pos.y << "," << v.col << ',' << v.uv.x << "," << v.uv.y << "]";
+      if (i < vtx_buffer.size() - 1) json_data << ",";
+    }
+
+    json_data << "],\"idx\":[";
+    for (int i = 0; i < idx_buffer.size(); ++i) {
+      auto id = idx_buffer[i];
+      json_data << id;
+      if (i < idx_buffer.size() - 1) json_data << ",";
+    }
+
+    json_data << "],\"cmd\":[";
+    for (int i = 0; i < cmd_buffer.size(); ++i) {
+      auto cmd = cmd_buffer[i];
+      json_data << "{\"cnt\":" << cmd.ElemCount << ", \"clp\":[" << cmd.ClipRect.x << "," << cmd.ClipRect.y << "," << cmd.ClipRect.z << "," << cmd.ClipRect.w << "]}";
+      if (i < cmd_buffer.size() - 1) json_data << ",";
+    }
+    json_data << "]}";
+    if (cmd_id < draw_data->CmdListsCount - 1) json_data << ",";
+  }
+
+  json_data << "]";
+
+  return json_data.str();
+}
+
+// records a video to a stream as a list of JSON frames
+bool recordVideoCallback(ImDrawData *draw_data, std::ostream& stream) {
+  const int framerate = 10;
+  const int max_frames = 20;
+  static bool first = true;
+  static int curr_frame = 0;
+  if (curr_frame % framerate == 0 && draw_data->CmdListsCount > 0 && curr_frame/framerate <= max_frames) {
+    if (first) {
+      stream << "[";
+    }
+    first = false;
+
+    stream << getFrameJSON(draw_data);
+
+    if (curr_frame/framerate < max_frames) {
+      stream << ",\n";
+    } else {
+      stream << "]" << std::endl;
+      std::cout << "Video recorded!" << std::endl;
+      curr_frame++;
+      return true;
+    }
+  }
+  curr_frame++;
+  return false;
 }
 
 /// @return true if we do not need to exit, false if we do.
@@ -77,7 +151,8 @@ bool pollGUI(void* context, std::function<void(void)> guiCallback)
     guiCallback();
   }
   ImGui::Render();
-  ImGui_ImplGlfwGL3_RenderDrawLists(ImGui::GetDrawData());
+  auto draw_data = ImGui::GetDrawData();
+  ImGui_ImplGlfwGL3_RenderDrawLists(draw_data);
   glfwSwapBuffers(window);
   return true;
 }
